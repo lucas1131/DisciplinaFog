437e460b82fba7caafd46b1b0e653d64b5ac3294
wqferr <wqferr@gmail.com>
2016-11-20 19:07:27 -0200

    Create Position class

---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,51 @@
+
+public class Position {
+
+    private int x {
+        get { return _x; }
+    };
+
+    private int y {
+        get { return _y; }
+    };
+
+
+    public Position(int x, int y) {
+        this._x = x;
+        this._y = y;
+    }
+
+    public Position(Position p) {
+        this._x = p.x;
+        this._y = p.y;
+    }
+
+    public bool IsContained(int xMax, int yMax) {
+        return this.x < xMax && this.y < yMax;
+    }
+
+    public bool IsContained(int xMin, int yMin, int xMax, int yMax) {
+        return this.x >= xMin && this.y >= yMin && this.IsContained(xMax, yMax);
+    }
+
+    public bool IsValid(BoardManager board) {
+        return this.IsContained(0, 0, board.rows, board.cols);
+    }
+
+    public static bool operator ==(Position p1, Position p2) {
+        return p1.x == p2.x && p1.y == p2.y;
+    }
+
+    public static bool operator !=(Position p1, Position p2) {
+        return p1.x != p2.x || p1.y != p2.y;
+    }
+
+    public static Position operator +(Position p1, Position p2) {
+        return new Position(p1.x + p2.x, p1.y + p2.y);
+    }
+
+    public static Position operator -(Position p1, Position p2) {
+        return new Position(p1.x - p2.x, p1.y - p2.y);
+    }
+
+}
