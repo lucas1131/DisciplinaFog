b2f3508c5b15722bb4765f026b8887d9490896bb
wqferr <wqferr@gmail.com>
2016-11-23 11:05:08 -0200

    Remove whitespace

--- Assets/Scripts/ClassStats.cs
+++ Assets/Scripts/ClassStats.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName="FE Class", menuName="Scriptable/FE Class", order=1)]
 public class ClassStats : ScriptableObject {
 
+    public string Name;
+
     [System.Serializable]
     public class Cost {
         [HideInInspector]
@@ -16,25 +18,25 @@ public class ClassStats : ScriptableObject {
     }
 
     public Cost[] MoveCost = new Cost[] {
-        new Cost ("Plains", 999),
-        new Cost ("Forest", 999),
-        new Cost ("Floor", 999),
-        new Cost ("Pillar", 999),
-        new Cost ("Mountain", 999),
-        new Cost ("Peak", 999),
-        new Cost ("Gate", 999),
-        new Cost ("Throne", 999),
-        new Cost ("Fort", 999),
-        new Cost ("River", 999),
-        new Cost ("Sea", 999),
-        new Cost ("Desert", 999),
-        new Cost ("Thicket", 999),
-        new Cost ("Wall", 999),
-        new Cost ("CrackedWall", 999),
-        new Cost ("Door", 999),
-        new Cost ("ClosedChest", 999),
-        new Cost ("OpenedChest", 999),
-        new Cost ("Switch", 999)
+        new Cost("Plains", 999),
+        new Cost("Forest", 999),
+        new Cost("Floor", 999),
+        new Cost("Pillar", 999),
+        new Cost("Mountain", 999),
+        new Cost("Peak", 999),
+        new Cost("Gate", 999),
+        new Cost("Throne", 999),
+        new Cost("Fort", 999),
+        new Cost("River", 999),
+        new Cost("Sea", 999),
+        new Cost("Desert", 999),
+        new Cost("Thicket", 999),
+        new Cost("Wall", 999),
+        new Cost("CrackedWall", 999),
+        new Cost("Door", 999),
+        new Cost("ClosedChest", 999),
+        new Cost("OpenedChest", 999),
+        new Cost("Switch", 999)
     };
 
     public int GetMovementCost(Terrains t) {
